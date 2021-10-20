ee2bb89d42dfcfd544aca1afc02dc3c84564520d
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-20 16:44:47 +0000

    Create components

--- react/react-course/src/App.js
+++ react/react-course/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>My Todos</h1>
+            <Todo text="Todo 1"/>
+            <Todo text="Todo 2"/>
+            <Todo text="Todo 3"/>
+
+        </div>
+    );
 }
 
 export default App;
