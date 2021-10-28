cbd78e5462e2b39d639caa28cd406114937b7f32
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-10-28 20:20:52 +0000

    fetching data from firebase

--- react/meetup/src/pages/AllMeetups.js
+++ react/meetup/src/pages/AllMeetups.js
@@ -1,26 +1,36 @@
 import MeetupList from "../components/meetups/MeetupList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function AllMeetupsPage() {
   const [isloading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://react-meetup-7bb76-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
 
-  fetch("https://react-meetup-7bb76-default-rtdb.firebaseio.com/meetups.json")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
-
-  if (isloading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
-  }
+    if (isloading) {
+      return (
+        <section>
+          <p>Loading...</p>
+        </section>
+      );
+    }
+  }, []);
 
   return (
     <section>
