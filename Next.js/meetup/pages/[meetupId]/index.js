48974dbff55639e0e6587bbbb2e3685c8f6fe4b0
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-11-08 02:08:13 +0000

    building a layout with float

---
+++ Next.js/meetup/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      address="Some address 5, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
+
+export default MeetupDetails;
