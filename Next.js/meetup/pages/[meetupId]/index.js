e745f4a67c461e72764141014e21562eea4ea99c
Alassane Ndoye <aloutndoye.an@gmail.com>
2021-11-12 23:52:40 +0000

    add static generation

--- Next.js/meetup/pages/[meetupId]/index.js
+++ Next.js/meetup/pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        title: "A First Meetup",
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
