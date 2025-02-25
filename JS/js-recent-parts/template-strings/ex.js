7b49e31ec5843a38a7520247c111f89b0d7fa743
Alassane Ndoye <aloutndoye.an@gmail.com>
2025-02-25 07:41:20 +0000

    Update ex.js

--- JS/js-recent-parts/template-strings/ex.js
+++ JS/js-recent-parts/template-strings/ex.js
@@ -1,10 +1,14 @@
-function upper(strings,...values) {}
+function upper(strings,...values) {
+	return strings[0] + values[0].toUpperCase() +
+	strings[1] + values[1].toUpperCase() +
+	strings[2] + values[2].toUpperCase() + strings[3];
+}
 
 var name = "kyle",
 	twitter = "getify",
 	topic = "JS Recent Parts";
 
 console.log(
-	`Hello ____ (@____), welcome to ____!` ===
+	upper`Hello ${name} (@${twitter}), welcome to ${topic}!` ===
 	"Hello KYLE (@GETIFY), welcome to JS RECENT PARTS!"
 );
