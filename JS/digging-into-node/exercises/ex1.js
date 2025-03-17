b6db1b49a514f170781f0aa0245d7dda563b4e51
aloundoye <aloutndoye.an@gmail.com>
2025-03-17 21:12:50 +0000

    command line scripts

--- JS/digging-into-node/exercises/ex1.js
+++ JS/digging-into-node/exercises/ex1.js
@@ -1,1 +1,42 @@
-// My first Node.js program
+#!/usr/bin/env node
+
+'use strict';
+import minimist from 'minimist';
+import getStdin from 'get-stdin';
+
+import path from 'path';
+import fs from 'fs';
+import util from 'util';
+
+const args = minimist(process.argv.slice(2), {
+  boolean: ['help', 'in'],
+  string: ['file'],
+});
+
+const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+
+if (args.help) {
+  console.log('Help needed');
+} else if (args.in || args._.includes('-')) {
+  // TODO: handle stdin
+  getStdin()
+    .then(processFile)
+    .catch((error) => {
+      console.error(error);
+    });
+} else if (args.file) {
+  const filepath = path.join(BASE_PATH, args.file);
+
+  fs.readFile(filepath, (err, contents) => {
+    if (err) {
+      console.error('Something wrong happened:', err.toString());
+    }
+    processFile(contents);
+  });
+} else {
+  console.error('Incorrect usage.\nHelp Needed');
+}
+
+function processFile(contents) {
+  process.stdout.write(contents.toString().toUpperCase());
+}
